b1da163c4c5d21685ca415f1d4cce6aa5f83b07b
Aaron J Kosel <aaronjkosel@gmail.com>
2023-10-21 14:04:19 -0400

    Improve documentation and the way credentials are passed in

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # yahoo-fantasy-sports
 
+### Setup
+
+You'll need to create an OAuth application to get the required `CLIENT_ID` and `CLIENT_SECRET`. Go [here](https://developer.yahoo.com/apps/create/) to do that. Application name and description can be anything. I set redirect uri to localhost:8000, just because it's required, but this won't be important. Then select 'Confidential Client' and then check 'Fantasy Sports'.
+
+### Environment Variables
+
+- `YAHOO_CLIENT_ID` - Client ID for your OAuth application
+- `YAHOO_CLIENT_SECRET` - Client Secret for your OAuth application
+- `YAHOO_LEAGUE_ID` - League ID which can be obtained from the URL for your league (e.g. https://football.fantasysports.yahoo.com/f1/123456, where 123456 is the league id)
+
 ### Usage
 
 ```
-credentials_file = '/path/to/credentials/file.json'
-league_id = '123456' # put your real league id here
-yfs = YahooFantasySports(credentials_file, league_id=league_id)
+yfs = YahooFantasySports()
 
 response = yfs.get_standings()
 ```
--- api.py
+++ api.py
@@ -4,29 +4,25 @@ import requests
 import webbrowser
 
 class YahooFantasySports:
-    def __init__(self, credentials_file, league_id=None):
-        self.credentials_file = open(credentials_file)
-        self.credentials = json.load(self.credentials_file)   
-        self.credentials_file.close()
-        
+    def __init__(self):
         self.authorize_url = 'https://api.login.yahoo.com/oauth2/request_auth'
         self.access_token_url = 'https://api.login.yahoo.com/oauth2/get_token'
-        
-        self.client_id = self.credentials['client_id']
-        self.client_secret = self.credentials['client_secret']
-        
+
+        self.client_id = os.environ.get('YAHOO_CLIENT_ID')
+        self.client_secret = os.environ.get('YAHOO_CLIENT_SECRET')
+
         self.redirect_uri = 'oob'
-        
-        self.league_id = league_id
+
+        self.league_id = os.environ.get('YAHOO_LEAGUE_ID')
         self.base_url = 'https://fantasysports.yahooapis.com/fantasy/v2'
-        
+
         tokens = self.retrieve_tokens()
-        
+
         if tokens:
             self.set_tokens(tokens)
         else:
             self.get_initial_tokens()
-                   
+
     def get_initial_tokens(self):
         data = {
             'client_id': self.client_id,
@@ -39,9 +35,9 @@ class YahooFantasySports:
 
         res = requests.post(self.authorize_url, params=data, headers={ 'Content-Type': 'application/json'})
         webbrowser.open(res.url)
-        
+
         code = input('Enter code: ')
-        
+
         data = {
             'client_id': self.client_id,
             'client_secret': self.client_secret,
@@ -55,11 +51,11 @@ class YahooFantasySports:
         self.set_tokens(response.json())
         self.cache_tokens()
 
-        
+
     def set_tokens(self, data):
         self.access_token = data['access_token']
         self.refresh_token = data['refresh_token']
-        
+
     def cache_tokens(self):
         tokens = {
             'access_token': self.access_token,
@@ -67,11 +63,11 @@ class YahooFantasySports:
         }
         with open('.tokens.json', 'w') as f:
             f.write(json.dumps(tokens))
-            
+
     def retrieve_tokens(self):
         if not os.path.exists('.tokens.json'):
             return None
-        
+
         with open('.tokens.json', 'r') as f:
             tokens = json.loads(f.read())
         return tokens
@@ -88,16 +84,16 @@ class YahooFantasySports:
 
         self.set_tokens(response.json())
         self.cache_tokens()
-        
+
     def get(self, path):
         url = '{}/{}'.format(self.base_url, path)
         response = requests.get(url, params={'format':'json'}, headers={ 'Authorization': 'Bearer {}'.format(self.access_token)})
         return response.json()
-        
+
     def get_standings(self):
         path = 'leagues;league_keys=nfl.l.{}/standings'.format(self.league_id)
         return self.get(path)
-    
+
     def get_roster(self, team_id):
         path = 'team/nfl.l.{}.t.{}/roster/players'.format(self.league_id, team_id)
         return self.get(path)
